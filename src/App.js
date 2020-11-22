ed0920e76a063ccc15f0f1eb3d7ae4edc37d7e12
kandasangrat6 <kandasangrat6@gmail.com>
2020-11-22 21:58:14 +0700

    re-init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/kandasangrat6/music-i-love"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          source code
         </a>
       </header>
     </div>
